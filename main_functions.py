2471d3910e0cb26ccb164588887d93feaf91890e
Ferran <badalferran@gmail.com>
2021-10-20 11:45:24 +0200

    hola

--- main_functions.py
+++ main_functions.py
@@ -23,7 +23,7 @@ def main2(size, titulo, azul):
     car.speed = 100
     # car = laneFollowingCar1()
 
-    screen.fill(WHITE)
+    screen.fill(azul)
 
     # -------- Main Program Loop -----------
     t = 0
@@ -38,6 +38,7 @@ def main2(size, titulo, azul):
             car.turn(-1)
         if keys[pygame.K_RIGHT]:
             car.turn(1)
+        if keys[pygame.K_q]:
         # print(t)
         # inputKey = input('press a key')
         # gameLoop(inputKey,car,screen)
