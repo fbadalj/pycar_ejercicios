b5e467a18e0a27641ac864a5c82de28d8fe01abd
Ferran <badalferran@gmail.com>
2021-10-26 22:16:11 +0200

    Break

--- main_functions.py
+++ main_functions.py
@@ -40,6 +40,7 @@ def main2(size, titulo, azul):
             car.turn(1)
         if keys[pygame.K_q]:
             break
+
         # print(t)
         # inputKey = input('press a key')
         # gameLoop(inputKey,car,screen)
